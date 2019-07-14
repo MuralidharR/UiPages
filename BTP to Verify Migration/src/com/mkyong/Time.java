75440ba32b4b960c0746660a70268a8a43c0323f
mural <mural@DESKTOP-NLS8CBB.home>
2019-07-13 22:56:03 -0600

    Committed for TimeStamp on UI

---
+++ BTP to Verify Migration/src/com/mkyong/Time.java
@@ -0,0 +1,22 @@
+package com.mkyong;
+
+import java.sql.Timestamp;
+import java.time.LocalDate;
+
+public class Time {
+
+    public static void main(String[] args) {
+
+        //  LocalDate to Timestamp
+        LocalDate now = LocalDate.now();
+        Timestamp timestamp = Timestamp.valueOf(now.atStartOfDay());
+
+        System.out.println(now);        // 2019-06-14
+        System.out.println(timestamp);  // 2019-06-14 00:00:00.0
+
+        //  Timestamp to LocalDate
+        LocalDate localDate = timestamp.toLocalDateTime().toLocalDate();
+        System.out.println(localDate);  // 2019-06-14
+
+    }
+}
